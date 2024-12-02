91188fdde3e5f9ce7f7ab074ba9a7be6f850bf25
iamhasibulhasan <hashibul218@gmail.com>
2024-12-02 14:46:12 +0600

    introduce applicaton layer

---
+++ src/Core/MinimalApi.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MinimalApi.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(configuraion =>
+            configuraion.RegisterServicesFromAssembly(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+
+
+            return services;
+        }
+    }
+}
