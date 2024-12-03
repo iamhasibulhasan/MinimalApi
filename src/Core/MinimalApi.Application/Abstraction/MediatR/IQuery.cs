5fdaa5e6a445610ae66f56903ea9b0ed014d0292
iamhasibulhasan <hashibul218@gmail.com>
2024-12-03 15:32:59 +0600

    query complete

--- src/Core/MinimalApi.Application/Abstraction/MediatR/IQuery.cs
+++ src/Core/MinimalApi.Application/Abstraction/MediatR/IQuery.cs
@@ -3,6 +3,10 @@ using MinimalApi.Application.Common.Utilities;
 
 namespace MinimalApi.Application.Abstraction.MediatR;
 
-public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+public interface IQueryResult<TResponse> : IRequest<Result<TResponse>>
+{
+}
+
+public interface IQuery<out TResponse> : IRequest<TResponse>
 {
 }
---
+++ src/Core/MinimalApi.Application/Features/Students/Queries/GetAll/GetAllStudentQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MinimalApi.Application.Abstraction.MediatR;
+using MinimalApi.Application.Common.Utilities;
+using MinimalApi.Application.ServiceInterfaces.Students;
+
+namespace MinimalApi.Application.Features.Students.Queries.GetAll;
+
+public sealed class GetAllStudentQueryHandler : IQueryHandler<GetAllStudentQuery, Result>
+{
+    private readonly IStudentService _studentService;
+    public GetAllStudentQueryHandler(IStudentService studentService)
+    {
+        _studentService = studentService;
+    }
+    public async Task<Result> Handle(GetAllStudentQuery request, CancellationToken cancellationToken)
+    {
+        return await _studentService.GetAllStudent(cancellationToken);
+    }
+}
